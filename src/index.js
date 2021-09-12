cac029d96f63f81a0259be9d8da956f82f76170d
Tomasz Rosik <Bosik27@gmail.com>
2021-09-12 17:35:25 +0200

    Add project structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './style.scss';
+import 'bootstrap';
+import { amountBtnEvent } from './assets/js/amountBtnEvent';
+import { initGliderInstance } from './assets/js/initGliderInstance';
+
+amountBtnEvent();
+initGliderInstance();
